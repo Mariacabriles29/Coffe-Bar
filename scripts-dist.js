71215b93f75e131786d6229f16a9d82e25636233
Mariacabriles29 <mecp2992@gmail.com>
2023-02-02 09:39:50 -0430

    adding header

---
+++ scripts-dist.js
@@ -0,0 +1,1 @@
+const toggleMenuElement=document.getElementById("toggle-menu"),mainMenuElement=document.getElementById("main-menu");toggleMenuElement.addEventListener("click",(()=>{mainMenuElement.classList.toggle("main-menu--show")}));
